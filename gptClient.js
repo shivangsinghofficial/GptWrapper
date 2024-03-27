186bd99895be9859b0f47998aa202e6467a09e3c
Shivang <Shivang.Singh@osb.co.uk>
2024-03-27 15:51:27 +0530

    Commented a block

--- gptClient.js
+++ gptClient.js
@@ -1,9 +1,9 @@
 const { Configuration, OpenAI } = require('openai');
 
 const apiKey = "";
-const openai = new OpenAI({
-    apiKey: `${apiKey}`, // This is the default and can be omitted
-  });
+// const openai = new OpenAI({
+//     apiKey: `${apiKey}`, // This is the default and can be omitted
+//   });
 
 async function interactWithGpt(prompt) {
     try {
